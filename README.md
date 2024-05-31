a3db5f6debaf4001e2f7e712f71bfbd82ab48b0e
j0ruge <joruge@gmail.com>
2024-05-30 22:14:41 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Testando a lib PDFME
+
+```bash
+npm i typescript --save-dev
+npx tsc --init
+```
+
+[Set Up a New TypeScript Project](https://www.digitalocean.com/community/tutorials/typescript-new-project)
---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+var world = 'world';
+function hello(who) {
+    if (who === void 0) { who = world; }
+    return "Hello ".concat(who, "! ");
+}
+exports.hello = hello;
+console.log(hello());
+console.log(hello());
